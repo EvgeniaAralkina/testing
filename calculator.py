28c44d693a1246b3b7488b4a834af9bb7ef00bd3
evgnm <71122410+EvgeniaAralkina@users.noreply.github.com>
2021-09-20 11:54:40 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -43,7 +43,7 @@ while (1):
     print('1 - fast mul\n2 - fast pow\n3 - fast square root\n')
     x = int(input('   ->'))
     a = int(input('a = '))
-    b = int(input('(степень корня) b = '))
+    b = int(input('(второй множитель/степнь/степень корня) b = '))
     if x == 1:
         print('a * b = ',fast_mul(a,b))
     elif x == 2:
